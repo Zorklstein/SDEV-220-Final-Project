bb0fcf6e77ad111958df5fdff140387d510c53e3
Zorklstein <Zbro1252@gmail.com>
2025-11-04 19:47:12 -0500

    employee class

--- SDEV 220 Final project.py
+++ SDEV 220 Final project.py
@@ -1,1 +1,21 @@
 #SDEV 220 Final project
+
+class Employee:
+    def __init__(self):
+        self.firstName = input("What is the employee's first name: ")
+        self.lastName = input("What is the employee's last name: ")
+        self.employeeID = input("What is the employee's ID number: ")
+        self.wage = float(input("How much does the employee make per hour: "))
+        self.dependents = input("How many dependants does the employee have: ")
+    
+    def __str__(self):
+        return (f'\nFirst Name: {self.firstName}\n' +
+                f'Last Name: {self.lastName}\n'+
+                f'Employee ID: {self.employeeID}\n'+
+                f'Payrate: ${self.wage:.2f}/hour\n'+
+                f'Number of Dependants: {self.dependents}\n')
+    
+p1 = Employee()
+p2 = Employee()
+print(p1, p2)
+
